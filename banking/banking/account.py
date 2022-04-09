a7490393308056f96526d896a5fc465053074dcf
Alessandro Lorenzi <me@alorenzi.eu>
2022-04-09 10:43:37 +0200

    refactor(banking): add transacions persitence

--- banking/banking/account.py
+++ banking/banking/account.py
@@ -1,12 +1,18 @@
 class Account:
     def __init__(self):
-        self._balance = 0
+        self._transactions = []
 
-    def deposit(self, amount: int):
-        self._balance += amount
+    def deposit(self, amount: int, date: str = ""):
+        self._add_transaction(amount, date)
 
-    def withdraw(self, amount: int):
-        self._balance -= amount
+    def withdraw(self, amount: int, date: str = ""):
+        self._add_transaction(-amount, date)
+
+    def _add_transaction(self, amount: int, date: str = ""):
+        self._transactions.append((amount, date))
 
     def get_balance(self) -> int:
-        return self._balance
+        balance = 0
+        for t in self._transactions:
+            balance += t[0]
+        return balance
