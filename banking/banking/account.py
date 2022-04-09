a06ebc4fd939da7c11bf063c3be853bb4b45eb56
Alessandro Lorenzi <me@alorenzi.eu>
2022-04-09 10:29:39 +0200

    feat(banking): account add balance

--- banking/banking/account.py
+++ banking/banking/account.py
@@ -1,6 +1,12 @@
 class Account:
+    def __init__(self):
+        self._balance = 0
+
     def deposit(self, amount: int):
-        pass
+        self._balance += amount
 
     def withdraw(self, amount: int):
-        pass
+        self._balance -= amount
+
+    def get_balance(self) -> int:
+        return self._balance
--- banking/tests/test_account.py
+++ banking/tests/test_account.py
@@ -6,3 +6,4 @@ class TestAccount:
         a = Account()
         a.deposit(500)
         a.withdraw(400)
+        assert 100 == a.get_balance()
